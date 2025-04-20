8180a45dfb1948687c817eec1d4e53cd636bfab5
utopy <30875147+utopyin@users.noreply.github.com>
2025-04-20 17:17:00 +0200

    feat: add token in options

--- packages/better-source/src/index.ts
+++ packages/better-source/src/index.ts
@@ -1,13 +1,15 @@
 type Options = {
 	key: string;
 	secret: string;
+	token: string;
 };
 
 export const betterSource = (options: Options) => {
-	const { key, secret } = options;
+	const { key, secret, token } = options;
 
 	return {
 		key,
 		secret,
+		token,
 	};
 };
